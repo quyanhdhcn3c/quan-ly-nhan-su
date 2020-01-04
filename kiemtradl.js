fb609768b3d1458ab86e9d8cf539f2a48cdaaa72
quyanhdhcn3c <58172737+quyanhdhcn3c@users.noreply.github.com>
2020-01-04 09:35:31 +0700

    Add files via upload

---
+++ kiemtradl.js
@@ -0,0 +1,26 @@
+﻿// JavaScript Document
+function kiemtradl()
+{
+	if(document.form1.txtuser.value.length == 0) 
+	{
+		window.alert(" Bạn phải nhập user ");
+		return false;
+	}
+	if(document.form1.txtpass.value.length == 0)
+	{
+		window.alert(" Bạn chưa điền mật khẩu ");
+		return false;
+	}
+	if(document.form1.txtuser.value.length < 4 )
+	{
+		window.alert(" Tên đăng nhập quá ngắn ");
+		return false;
+	}
+	if(document.form1.txtpass.value.length < 4)
+	{
+		window.alert(" Mật khẩu quá ngắn ");
+		return false;
+	}
+	return true;	
+	
+}
